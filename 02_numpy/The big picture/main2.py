89a6bac947e7d2c1c920c0f5270ae5de36cb0004
aayush <hiaayush30@gmail.com>
2025-09-27 00:55:57 +0530

    commit

--- 02_numpy/The big picture/main2.py
+++ 02_numpy/The big picture/main2.py
@@ -2,7 +2,7 @@ import json
 
 
 def load_data(file_path):
-    with open(file_path) as f:
+    with open(file_path, "r") as f:
         data = json.load(f)
         return data
 
@@ -36,14 +36,46 @@ def people_you_may_know(id, data):
                 # don't show me the original user and don't show me those who are already friends with the original user
                 # count mutual friends
                 suggestions[mutual] = suggestions.get(mutual, 0) + 1
-    print(suggestions)
+
+    # print(suggestions)
     sorted_suggestions = sorted(suggestions.items(), key=lambda x: x[1], reverse=True)
-    # suggestions.items() will return key value pairs return the values (x[1]) and sort them
-    print(sorted_suggestions)
-    return [user_id for user_id, _ in sorted_suggestions]
+    # suggestions.items() will give key value pairs from which use value (x[1]) to sort them
+    # sorted() always returns a list
+    # print(sorted_suggestions)
+    return [user_id for user_id, score in sorted_suggestions]
 
 
-data = load_data("data.json")
+def pages_you_may_like(id, data):  # user id
+    users_pages = {}
+    for user in data["users"]:
+        users_pages[user["id"]] = set(user["liked_pages"])
+
+    if id not in users_pages:
+        return []
+
+    already_liked = users_pages[id]
+    suggestions = {}
+
+    for other_user, pages in users_pages.items():
+        if other_user != id:
+            shared_pages = already_liked.intersection(
+                pages
+            )  # overlapping pages which both like
+        for page in pages:
+            if page not in already_liked:
+                suggestions[page] = suggestions.get(page, 0) + len(shared_pages)
+                # score is the number pages both the users already like
+
+    sorted_suggestions = sorted(suggestions.items(), key=lambda x: x[1], reverse=True)
+    return [
+        x[1] for x in sorted_suggestions if x[1] != 0
+    ]  # ignore pages whose score is 0
+
+
+data = load_data("massive_data.json")
 # display_users(data)
-recc = people_you_may_know(1, data)
-print(recc)
+recc_people = people_you_may_know(3, data)
+print(recc_people)
+
+recc_pages = pages_you_may_like(1, data)
+print(recc_pages)
