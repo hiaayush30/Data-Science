4e8ebd17af24ee6969a11c9b0f22a313c19a7933
aayush <hiaayush30@gmail.com>
2025-09-27 13:01:12 +0530

    commit

--- 02_numpy/cwh/main2.py
+++ 02_numpy/cwh/main2.py
@@ -5,7 +5,7 @@ arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
 # indexing and slicing
 
 flat = arr.flatten()
-print(flat[4])
+print(flat[4])  # indexing
 print(flat[2:5])
 print(flat[:5])
 print(flat[0::2])  # every second element
@@ -16,3 +16,22 @@ print(flat[0::2])  # every second element
 b = flat[3:7]
 b[0] = 444
 print(flat)
+
+
+# but i want a copy
+
+b = flat[1:3].copy()
+b[0] = 22878
+print(flat)
+
+# fancy idexing
+
+indexes = [0, 1, 2, 5]
+arr = np.array([x for x in range(100)])
+
+selected = arr[indexes]
+selected[0] = 2381
+
+# boolean masking
+mask = arr > 80
+print(arr[mask])
