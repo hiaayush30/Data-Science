1db6e2653f7a5dc7bd0db218c2ba06713d21f972
aayush <hiaayush30@gmail.com>
2025-10-02 14:02:40 +0530

    numpy

--- 02_numpy/cwh/main2.py
+++ 02_numpy/cwh/main2.py
@@ -6,23 +6,23 @@ arr = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]])
 
 flat = arr.flatten()
 print(flat[4])  # indexing
-print(flat[2:5])
-print(flat[:5])
-print(flat[0::2])  # every second element
+# print(flat[2:5])
+# print(flat[:5])
+# print(flat[0::2])  # every second element
 
 # slicing returns a view not a copy unlike lists in python (which returns a copy)
 # this is done by numpy to avoid unnecessary copies, making operations faster and saving memory
 
 b = flat[3:7]
 b[0] = 444
-print(flat)
+# print(flat)
 
 
 # but i want a copy
 
 b = flat[1:3].copy()
 b[0] = 22878
-print(flat)
+# print(flat)
 
 # fancy idexing
 
@@ -34,4 +34,4 @@ selected[0] = 2381
 
 # boolean masking
 mask = arr > 80
-print(arr[mask])
+# print(arr[mask])
